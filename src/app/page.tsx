3fbe6764616507b076520c9952d263a6cabef505
Zephyrrr <kuangdadu@gmail.com>
2025-05-10 05:12:22 +0000

    Refactor Home component by removing unused imports and simplifying content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { HomeActions } from "@/components/home-actions";
-import { Container, Flex, Kbd, Link, Separator, Text } from "@radix-ui/themes";
+import { Container, Flex } from "@radix-ui/themes";
 import Image from "next/image";
 
 export default function Home() {
@@ -14,36 +14,7 @@ export default function Home() {
             height="120"
             className="invert dark:invert-0 mt-8 mb-2"
           />
-          <Text as="p">
-            Welcome to the LiveKit livestream demo app. You can join or start
-            your own stream. Hosted on{" "}
-            <Link href="https://livekit.io/cloud" target="_blank">
-              LiveKit Cloud
-            </Link>
-            . Bored of the current color theme? Switch it up by pressing{" "}
-            <Kbd>⌘&thinsp;C</Kbd> !
-          </Text>
           <HomeActions />
-          <Separator orientation="horizontal" size="4" className="my-2" />
-          <Text as="p" size="2">
-            Feel free to clone this full-stack NextJS app{" "}
-            <Link
-              href="https://github.com/livekit-examples/livestream"
-              target="_blank"
-            >
-              here
-            </Link>
-            .
-            {/* And also be sure to check out clone our{" "}
-            <Link
-              href="https://github.com/livekit-examples/swift-livestream"
-              target="_blank"
-            >
-              iOS
-            </Link>{" "}
-            and <Link href="https://github.com/livekit-examples/android-livestream" target="_blank">Android</Link> clients, which are compatible with
-            this web app! */}
-          </Text>
         </Flex>
       </Container>
     </main>
